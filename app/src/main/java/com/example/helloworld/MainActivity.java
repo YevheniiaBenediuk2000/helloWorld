4cdec75bb08adba0de739ac0f11208243da1173f
yevheniiabenediuk <yevheniiabenediuk@gmail.com>
2023-09-25 10:48:22 +0300

    Change color of text

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView TvHelloWorld;
+    Button btnChangeColor;
 
     private TextView tvHelloWorld;
 
@@ -17,13 +20,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initializing
+        Button btnChangeColor = findViewById(R.id.btnChangeColor);
+        Button btnChangeText = (Button) findViewById(R.id.btnChangeText);
         this.tvHelloWorld = findViewById(R.id.tvHelloWorld);
 
-        Button btnChangeText = (Button) findViewById(R.id.btnChangeText);
+        // Changing the color
+        btnChangeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvHelloWorld.setTextColor(Color.parseColor("#d54b1a"));
+            }
+        });
+
         btnChangeText.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 tvHelloWorld.setText("Hello Kaunas!");
             }
         });
     }
+
+    // Color change
+//    Button btnChangeColor = findViewById(R.id.btnChangeColor);
+
+
+
 }
