c7fc3b279812be2abfbcf61ce28c9d34ae6899bb
yevheniiabenediuk <yevheniiabenediuk@gmail.com>
2023-09-25 09:56:45 +0300

    Added changed text functionality

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvHelloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvHelloWorld = findViewById(R.id.tvHelloWorld);
     }
 
     public void onBtnChangeTextClick(View view) {
-        Toast.makeText(this, "Java is amazing!", Toast.LENGTH_LONG).show();
+//        Toast.makeText(this, "Java is amazing!", Toast.LENGTH_LONG).show();
+        this.tvHelloWorld.setText("Hello Kaunas!");
     }
 }
